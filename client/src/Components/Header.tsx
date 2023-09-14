26ff38ec3af56db79fb8903f44591b305496ea4d
Camh-git <cameronhillier1@gmail.com>
2023-09-14 23:03:06 +0100

    feat: navbar
    Added a header with the logo and navbar
    added some css to make the header look closer to the sketch

--- client/src/Components/Header.tsx
+++ client/src/Components/Header.tsx
@@ -1,4 +1,14 @@
-import logo from "./Fakelandia_DOJ_logo.png";
-const navHeader: React.FC = () => <>Header!</>;
+import logo from "../Fakelandia_DOJ_logo.png";
+import { NavLink } from "react-router-dom";
+const navHeader: React.FC = () => (
+  <header>
+    <img src={logo} alt="logo" className="HeaderLogo" />
+    <nav style={{ width: "100%" }}>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/confess">confess</NavLink>
+      <NavLink to="/Misdomeanours">Misdomeanours</NavLink>
+    </nav>
+  </header>
+);
 
 export default navHeader;
