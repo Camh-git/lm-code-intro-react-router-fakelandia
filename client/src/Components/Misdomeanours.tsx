6a8edababb6dfe3f85f8b7ccf4b169068518a8d2
Camh-git <cameronhillier1@gmail.com>
2023-09-15 21:29:34 +0100

    Feat: fetch misdomeanours
    Added the useEffect hook, state and fetch required to get the data from the api.

--- client/src/Components/Misdomeanours.tsx
+++ client/src/Components/Misdomeanours.tsx
@@ -1,3 +1,28 @@
-const Misdomeanours: React.FC = () => <>Misdomeanours</>;
+import { useEffect, useState } from "react";
+import { Misdemeanour } from "../types/misdemeanours.types";
+
+const Misdomeanours: React.FC = () => {
+  const [incidents, setIncidents] = useState<Array<Misdemeanour>>();
+  useEffect(() => {
+    const list = async () => {
+      const result = await fetch("http://localhost:8080/api/misdemeanours/3");
+      result.json().then((json) => {
+        setIncidents(json.misdemeanours);
+        /*console.log("json");
+        console.log(json);
+        console.log("state:");
+        console.log(incidents);//keep until done, for when it inevitably breaks */
+      });
+    };
+    list();
+  }, []);
+  return (
+    <>
+      <h2>Misdomeanours</h2>
+      <p>{incidents === undefined && "Loading..."}</p>
+      <p>{incidents && incidents[0].citizenId}</p>
+    </>
+  );
+};
 
 export default Misdomeanours;
