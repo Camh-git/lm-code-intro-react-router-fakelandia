f8e914dd1e86fe23e98079e62d46071e293d494f
Camh-git <cameronhillier1@gmail.com>
2023-09-15 21:49:12 +0100

    Feat: table
    added a simple version of the table, will update to use context and a sub component that loops through the entries when time allows.
    added a tiny amount of css to the table to make it look decent

---
+++ client/src/components/misdemeanourList.tsx
@@ -0,0 +1,7 @@
+const misdemeanourList: React.FC = () => (
+  <>
+    TODO: this should take in the misdomeaners list and return a nice table to
+    replace the placeholder
+  </>
+);
+export default misdemeanourList;
