4f83540fc63d4021347177e1938b95cf6295f92a
Camh-git <cameronhillier1@gmail.com>
2023-10-04 22:58:54 +0100

    feat: filter
    implementing the filter.
    Note: there is currently a bug where the filter will block everything until you  change it to something other than none and then back to none, I have attempted to remedy this by updating the filter after calling the list but that doesn't appear to work.
    used this approach rather than the more sensible setvisibleList([...visibleList,incident])
    because of error: Type 'Misdemeanour[] | undefined' is not an array type. any comments on how to resolve this would be appreciated.

--- client/src/components/misdemeanours.tsx
+++ client/src/components/misdemeanours.tsx
@@ -1,23 +1,53 @@
 import { useContext, useEffect, useState } from "react";
-import { Misdemeanour } from "../types/misdemeanours.types";
+import { Misdemeanour, MisdemeanourKind } from "../types/misdemeanours.types";
 import MisdemeanourContext from "../hooks/misdemeanour_context";
 
 const Misdomeanours: React.FC = () => {
   const incidentContext = useContext(MisdemeanourContext);
   const [incidents, setIncidents] = useState<Array<Misdemeanour>>();
+  const [visibleList, setvisibleList] = useState<Array<Misdemeanour>>();
+  const [filter, setFilter] = useState("none");
   useEffect(() => {
     const list = async () => {
       const result = await fetch("http://localhost:8080/api/misdemeanours/3");
       const json = await result.json();
       setIncidents(json.misdemeanours);
       incidentContext.setMisdemeanours(incidents);
-      //console.log(json); //Sanity check
     };
     list();
+    setFilter("none");
   }, []);
+
+  useEffect(() => {
+    if (filter === "none") {
+      console.log("unfiltered");
+      setvisibleList(incidents);
+    } else {
+      let visible: Misdemeanour[] = [];
+      incidents?.forEach((incident) => {
+        if (incident.misdemeanour === filter) {
+          visible.push(incident);
+        }
+      });
+      setvisibleList(visible);
+    }
+  }, [filter]);
+
   return (
     <>
       <h1>Misdemeanours</h1>
+      <label htmlFor="filterOptions">Filter misdemeanours:</label>
+      <select
+        name="filterOptions"
+        defaultValue="none"
+        onChange={(e) => setFilter(e.target.value)}
+      >
+        <option value="none">No filter</option>
+        <option value="rudeness">Mild Public Rudeness</option>
+        <option value="vegetables">Not eating your vegetables</option>
+        <option value="lift">Talking in a lift</option>
+        <option value="united">Supporting manchester united</option>
+      </select>
       <p>{incidents === undefined && "Loading..."}</p>
       {incidents && (
         <table>
@@ -28,36 +58,22 @@ const Misdomeanours: React.FC = () => {
               <th>Misdemeanour</th>
               <th>Punishment idea</th>
             </tr>
-            <tr>
-              <td>{incidents[0].citizenId}</td>
-              <td>{incidents[0].date}</td>
-              <td>{incidents[0].misdemeanour}</td>
-              <td>
-                <img
-                  src="https://picsum.photos/190/100"
-                  alt="punishment idea"
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>{incidents[1].citizenId}</td>
-              <td>{incidents[1].date}</td>
-              <td>{incidents[1].misdemeanour}</td>
-              <td>
-                <img src="https://picsum.photos/190/99" alt="punishment idea" />
-              </td>
-            </tr>
-            <tr>
-              <td>{incidents[2].citizenId}</td>
-              <td>{incidents[2].date}</td>
-              <td>{incidents[2].misdemeanour}</td>
-              <td>
-                <img
-                  src="https://picsum.photos/190/101"
-                  alt="punishment idea"
-                />
-              </td>
-            </tr>
+            {visibleList?.map((index) => {
+              return (
+                <tr>
+                  <td>{index.citizenId}</td>
+                  <td>{index.date}</td>
+                  <td>{index.misdemeanour}</td>
+                  <td>
+                    <img
+                      //TODO: try and find what the valid range is so you can call some different images
+                      src="https://picsum.photos/190/100"
+                      alt="punishment idea"
+                    />
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       )}
