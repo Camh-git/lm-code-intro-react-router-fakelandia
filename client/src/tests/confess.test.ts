fa0b1631fff472d93acca2e7d31b9d6b8a3de78c
Camh-git <cameronhillier1@gmail.com>
2023-10-04 22:08:52 +0100

    feat: test framework
    I have attempted to add some tests, however I am running into an issue where they will allways fail because they don't expect the export to be there, but if it isn't the app throws this error and doesn't render:
    'confess.test.ts' cannot be compiled under '--isolatedModules' because it is considered a global script file. Add an import, export, or an empty 'export {}' statement to make it a module.

---
+++ client/src/tests/confess.test.ts
@@ -0,0 +1,16 @@
+import Confess from "../components/confess";
+describe("Test the validation", () => {
+  test("Check the min subject length", () => {
+    expect("").toBe("");
+  });
+  test("Check that the reason is one of the 5 valid numbers", () => {
+    expect("").toBe("");
+  });
+  test("Check the min description length", () => {
+    expect("").toBe("");
+  });
+  test("Check that the button is disabled at the right time", () => {
+    expect("").toBe("");
+  });
+});
+export {};
---
+++ client/src/tests/misdemeanours.test.ts
@@ -0,0 +1,10 @@
+import Misdomeanours from "../components/misdemeanours";
+describe("Testing the fetch", () => {
+  test("Check that the fetch returns 3 entries", () => {
+    expect("").toBe("");
+  });
+  test("Check that the entries for citizen ID are valid", () => {
+    expect("").toBe("");
+  });
+});
+export {};
